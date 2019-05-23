9c4b03bf661e909a9e328618eb07241a8dda091d
hackerr101 <abhinav.edulakanti@gmail.com>
2019-05-23 09:13:22 +0530

    new files

---
+++ evenorodd.js
@@ -0,0 +1,9 @@
+ var enter = prompt("enter a number");
+if (enter%2==0);
+{
+    console.log("it is an even number");
+}
+if (enter%2!=0)
+{
+    console.log("it is an odd number");
+}
---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+for (var i=1; i<=100; i++);
+if (i%3==o);
+{
+    consoe.log("fizz");
+}
+ if (i%5==o);
+ {
+     consol.log("buzz");
+ }
+  if (i%3==o && i%5==0);
+  {
+    console.log("fizzbuzz");
+  }
+  
